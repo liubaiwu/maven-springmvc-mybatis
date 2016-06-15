0bcde2d58cda35731821721d6b600374e0acf614
liubaiwu <286067198@qq.com>
2016-06-15 16:07:35 +0800

    初次提交。

---
+++ src/main/java/cn/springmvc/controller/UserController.java
@@ -0,0 +1,17 @@
+package cn.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+	@Controller
+	@RequestMapping("/")
+	public class UserController {
+
+		@RequestMapping("index.do")
+		public String index(){
+			return "index";
+		}
+		
+	}
+
