054e17e077bba29ffff4f46cee4bf5d2a20d5862
look <286067198@qq.com>
2016-10-24 11:07:38 +0800

    更新

---
+++ src/test/java/cn/springmvc/test/te.java
@@ -0,0 +1,14 @@
+package cn.springmvc.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class te {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
